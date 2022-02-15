99537ac6883afc96bd090d03ab5a2bc30ffea0c7
Medina <imn.medina@icloud.com>
2022-02-15 23:03:17 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Hero[] attackTypes = {new Magic(), new Warrior(), new Medic()};
+
+        for (int i = 0; i < attackTypes.length; i++) {
+            attackTypes[i].applySuperAbility();
+            if (attackTypes[i] instanceof Medic) {
+                ((Medic) attackTypes[i]).increase_experience();
+            }
+        }
+    }
+}
---
+++ src/com/company/Warrior.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Warrior extends Hero{
+
+    public void applySuperAbility() {
+        System.out.println("Warrior нанес критический урон Боссу");
+    }
+}
